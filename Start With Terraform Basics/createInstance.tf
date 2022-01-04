6c7ebad80ff8435485370a61b54b39385497b1a1
Vijay Chaudhery <vijay_c43@outlook.com>
2022-01-04 19:07:09 +0530

    update2

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,4 +1,9 @@
-
+provider "aws" {
+    version = "3.53.0"
+    access_key = "AKIATRDKZRSS7VTBOOW5"
+    secret_key = "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"
+    region     = "us-east-2"
+}
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-05803413c51f242b7"
   instance_type = "t2.micro"
