a63e0e53aee9ff22d0ce5a078b2436b66439598a
Vijay Chaudhery <vijay_c43@outlook.com>
2022-01-04 18:59:49 +0530

    update

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,10 +1,6 @@
 
 resource "aws_instance" "MyFirstInstnace" {
-  count         = 3
-  ami           = "ami-0bff25b43a4479334"
-  instance_type = "t4g.micro"
+  ami           = "ami-05803413c51f242b7"
+  instance_type = "t2.micro"
 
-  tags = {
-    Name = "demoinstnce-${count.index}"
-  }
 }
