55208a7a445e88bb8bf964b3f6ae1c9cd45d706b
Vijay Chaudhery <vijay_c43@outlook.com>
2022-01-05 14:38:18 +0530

    update form laptop

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-    access_key = "AKIATRDKZRSSQKI6JM6G"
-    secret_key = "iuw/VqnI5iQosdH0lbwLvh5evFSHi8kUpBByQgwf"
-    region     = "us-east-2"
-}
 
 resource "aws_instance" "example" {
   ami           = "ami-05803413c51f242b7"
